7365c089b56fccc1d567a48e95346d85d77a7185
Babluparmar <145090309+Babluparmar@users.noreply.github.com>
2023-10-03 18:52:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let textarea = document.getElementById("textbox");
+
+textarea.addEventListener("input", function () {
+    var text = this.value;
+    let char = text.length;
+    document.getElementById("char").innerHTML = char;
+
+    text = text.trim();
+    let words = text.split(" ");
+    let cleanlist = words.filter(function (elm) {
+        return elm != "";
+    });
+    document.getElementById("word").innerHTML = cleanlist.length;
+
+});
